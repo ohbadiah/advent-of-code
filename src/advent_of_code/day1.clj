ea0a5f27c7cc39b7c184e00df9522a8455923a16
Nick McAvoy <nick@crossbeam.com>
2021-12-16 21:58:16 -0500

    Zprint

--- src/advent_of_code/day1.clj
+++ src/advent_of_code/day1.clj
@@ -1,39 +1,34 @@
 (ns advent-of-code.day1)
 
-(def input  (-> "resources/input/day1"
-                slurp
-                (.split "\\n")
-                vec))
+(def input
+  (-> "resources/input/day1"
+      slurp
+      (.split "\\n")
+      vec))
 
-(defn count-increases [ipt]
+(defn count-increases
+  [ipt]
   (reduce (fn [{:keys [last-element increase-count]} el]
             (let [as-int (Integer/parseInt el)]
-              {:last-element as-int
+              {:last-element as-int,
                :increase-count (if (and last-element (> as-int last-element))
                                  (inc increase-count)
                                  increase-count)}))
-          {:last-element nil
-           :increase-count 0}
-          ipt
-          ))
+    {:last-element nil, :increase-count 0}
+    ipt))
 
-(count-increases )
+(count-increases)
 (:increase-count (count-increases input))
 
-(defn zip-with-self-offset-1 [ipt]
-  (map vector ipt (concat [nil] ipt)))
+(defn zip-with-self-offset-1 [ipt] (map vector ipt (concat [nil] ipt)))
 
 
-(def test-ipt ["1" "2" "3" "0"] )
-(defn count-increases-zipped [ipt]
+(def test-ipt ["1" "2" "3" "0"])
+(defn count-increases-zipped
+  [ipt]
   (let [ints (map #(Integer/parseInt %) ipt)
         zipped (zip-with-self-offset-1 ints)
         increases (map (fn [[fst snd]]
-                         (if (and snd (> fst snd))
-                           "increase"
-                           "not-increase"))
-                       zipped)]
+                         (if (and snd (> fst snd)) "increase" "not-increase"))
+                    zipped)]
     (count (filter (partial = "increase") increases))))
-
-
-keep-indexed
--- src/advent_of_code/day15.clj
+++ src/advent_of_code/day15.clj
@@ -1,53 +1,43 @@
 (ns advent-of-code.day15)
 
-(defn num-string-to-num-vec [num-string]
+(defn num-string-to-num-vec
+  [num-string]
   (->> num-string
-     seq
-     (map (comp #(Integer/parseInt %) str))))
+       seq
+       (map (comp #(Integer/parseInt %) str))))
 
-(def input  (let [list-of-strs  (-> "resources/input/day15"
-                                    slurp
-                                    (.split "\\n")
-                                    vec)]
-              (->> list-of-strs
-                   (map num-string-to-num-vec)
-                   to-array-2d)))
+(def input
+  (let [list-of-strs (-> "resources/input/day15"
+                         slurp
+                         (.split "\\n")
+                         vec)]
+    (->> list-of-strs
+         (map num-string-to-num-vec)
+         to-array-2d)))
 
 (def start-pos [0 0])
 (def end-pos [99 99])
 
-(defn move-right [[x y]]
-  [(inc x) y])
+(defn move-right [[x y]] [(inc x) y])
 
-(defn move-down [[x y]]
-  [x (inc y)])
+(defn move-down [[x y]] [x (inc y)])
 
-(defn get-at-coordinate [ipt [x y]]
-  (try
-    (aget ipt x y)
-    (catch java.lang.ArrayIndexOutOfBoundsException e
-      10)))
+(defn get-at-coordinate
+  [ipt [x y]]
+  (try (aget ipt x y) (catch java.lang.ArrayIndexOutOfBoundsException e 10)))
 
 #_(get-at-coordinate input [1000 0])
 
-
-(defn run-sim [ipt] (loop [pos start-pos
-                        cost-of-trip 0]
-                   (let [look-right (get-at-coordinate ipt (move-right pos))
-                         look-down (get-at-coordinate ipt (move-down pos))]
-                     (cond
-                       (= pos end-pos)
-                       (do
-                         (println "We reached the end!")
-                         cost-of-trip)
-
-                       (< look-right look-down)
-                       (do
-                         (println (format "Moving right from %s" pos))
-                         (recur (move-right pos) (+ cost-of-trip look-right)))
-
-                       :else
-                       (do
-                         (println (format "Moving down from %s" pos))
-                         (recur (move-down pos) (+ cost-of-trip look-down)))))))
+(defn run-sim
+  [ipt]
+  (loop [pos start-pos
+         cost-of-trip 0]
+    (let [look-right (get-at-coordinate ipt (move-right pos))
+          look-down (get-at-coordinate ipt (move-down pos))]
+      (cond (= pos end-pos) (do (println "We reached the end!") cost-of-trip)
+            (< look-right look-down)
+              (do (println (format "Moving right from %s" pos))
+                  (recur (move-right pos) (+ cost-of-trip look-right)))
+            :else (do (println (format "Moving down from %s" pos))
+                      (recur (move-down pos) (+ cost-of-trip look-down)))))))
 #_(run-sim input)
